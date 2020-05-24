843d42e8ca2b037ebc06ffa5c8650dc6e7383de3
Vitaliy Tsirkunov <vitaliy@tsirkunov.ru>
2020-05-24 12:41:51 +0300

    Добавлен проект приложения для Jetson Nano.

--- MotorDriver/MotorDriver.ino
+++ MotorDriver/MotorDriver.ino
@@ -1,52 +1,98 @@
 // Прошивка драйвера двигателей. Версия 4.0.
 
-#include <Servo.h> 
- 
 const int LEFT_MOTOR_DIR_PIN = 7;
 const int LEFT_MOTOR_PWM_PIN = 9;
 const int RIGHT_MOTOR_DIR_PIN = 8;
 const int RIGHT_MOTOR_PWM_PIN = 10;
 
+const int CAMERA_LEFT_RIGHT_SERVO_PIN = 12;
+const int CAMERA_UP_DOWN_SERVO_PIN = 13;
+
 // Этими константами можно выравнять движение вперед, если робот сремится завернуть вправо или влево из-за того, что двигатели разные.
-const byte LEFT_MOTOR_SPEED = 100;
-const byte RIGHT_MOTOR_SPEED = 100;
+const int LEFT_MOTOR_SPEED = 500;
+const int RIGHT_MOTOR_SPEED = 500;
 
 // Эти константы задают крайние положения для сервоприводов
-const byte CAMERA_EXTREME_LEFT = 180;
-const byte CAMERA_CENTER = 90;
-const byte CAMERA_EXTREME_RIGHT = 0;
-const byte CAMERA_EXTREME_DOWN = 42;
-const byte CAMERA_STRAIGHT_FORWARD = 75;
-const byte CAMERA_EXTREAM_UP = 180;
-const byte CAMERA_STEP = 3;
+const byte CAMERA_EXTREME_LEFT = 122;
+const byte CAMERA_CENTER = 73;
+const byte CAMERA_EXTREME_RIGHT = 25;
+const byte CAMERA_EXTREME_DOWN = 48;
+const byte CAMERA_STRAIGHT_FORWARD = 66;
+const byte CAMERA_EXTREAM_UP = 122;
 
-Servo cameraLeftRight;
-Servo cameraUpDown;
+int timerCounter = 0;
+int cameraLeftRightServo = CAMERA_CENTER;
+int cameraUpDownServo = CAMERA_STRAIGHT_FORWARD;
+int leftMotor = 0;
+int rightMotor = 0;
+
+// Прерывание таймера.
+SIGNAL (TIMER1_COMPA_vect){
+  if(timerCounter == cameraLeftRightServo){
+    digitalWrite (CAMERA_LEFT_RIGHT_SERVO_PIN, LOW);
+  }
+
+  if(timerCounter == cameraUpDownServo){
+    digitalWrite (CAMERA_UP_DOWN_SERVO_PIN, LOW);
+  }
+
+  if(timerCounter == leftMotor){
+    digitalWrite (LEFT_MOTOR_PWM_PIN, LOW);
+  }
+
+  if(timerCounter == rightMotor){
+    digitalWrite (RIGHT_MOTOR_PWM_PIN, LOW);
+  }
+
+  timerCounter++;
+
+  if(timerCounter == 1000){
+    timerCounter = 0;
+
+    if(cameraLeftRightServo){
+      digitalWrite (CAMERA_LEFT_RIGHT_SERVO_PIN, HIGH);
+    }
+    if(cameraUpDownServo){
+      digitalWrite (CAMERA_UP_DOWN_SERVO_PIN, HIGH);
+    }
+    if(leftMotor){
+      digitalWrite (LEFT_MOTOR_PWM_PIN, HIGH);
+    }
+    if(rightMotor){
+      digitalWrite (RIGHT_MOTOR_PWM_PIN, HIGH);
+    }
+  }
+   
+}
 
 //----------------------------------------------------------
 void setup()
 {
+    // Инициализируем 16 битный таймер
+    TCCR1A = 0;                          // Сче 
+    TCCR1B = _BV(CS11) | _BV(WGM12);     // Предделитель 8, то есть таймер будет работать на частоте в 8 раз меньше, чем основная частота.
+    TCNT1 = 0;              // clear the timer count
+    TIFR |= _BV(OCF1A);      // clear any pending interrupts;
+    TIMSK |=  _BV(OCIE1A) ;  // enable the output compare interrupt
+    OCR1A = 39;
+
+    
     // Setup the pins
     pinMode( LEFT_MOTOR_DIR_PIN, OUTPUT );
     pinMode( LEFT_MOTOR_PWM_PIN, OUTPUT );
     pinMode( RIGHT_MOTOR_DIR_PIN, OUTPUT );
     pinMode( RIGHT_MOTOR_PWM_PIN, OUTPUT );
-
-    cameraLeftRight.attach(12);
-    cameraLeftRight.write(CAMERA_CENTER);
-    cameraUpDown.attach(13);
-    cameraUpDown.write(CAMERA_STRAIGHT_FORWARD);
+    pinMode( CAMERA_LEFT_RIGHT_SERVO_PIN, OUTPUT );
+    pinMode( CAMERA_UP_DOWN_SERVO_PIN, OUTPUT );
 
     Serial.begin(9600, SERIAL_8N1);
     while (!Serial) {}
-    
+
 }
  
 //----------------------------------------------------------
 void loop()
 {
-  byte pos;
-  
   if(Serial.available() > 0){
   
     // Чтение команды
@@ -58,86 +104,76 @@ void loop()
         // Двигаемся вперед
         digitalWrite( LEFT_MOTOR_DIR_PIN, HIGH );
         digitalWrite( RIGHT_MOTOR_DIR_PIN, HIGH );
-        analogWrite( LEFT_MOTOR_PWM_PIN, LEFT_MOTOR_SPEED );
-        analogWrite( RIGHT_MOTOR_PWM_PIN, RIGHT_MOTOR_SPEED );
+        leftMotor = LEFT_MOTOR_SPEED;
+        rightMotor = RIGHT_MOTOR_SPEED;
         break;
 
       case 'r':
         // Поворачиваем направо
-        digitalWrite( LEFT_MOTOR_DIR_PIN,  LOW);
-        digitalWrite( RIGHT_MOTOR_DIR_PIN, HIGH );
-        analogWrite( LEFT_MOTOR_PWM_PIN, 128 );
-        analogWrite( RIGHT_MOTOR_PWM_PIN, 128 );
+        digitalWrite( LEFT_MOTOR_DIR_PIN, HIGH );
+        digitalWrite( RIGHT_MOTOR_DIR_PIN, LOW );
+        leftMotor = LEFT_MOTOR_SPEED;
+        rightMotor = RIGHT_MOTOR_SPEED;
         break;
 
       case 'l':
         // Поворачиваем налево
-        digitalWrite( LEFT_MOTOR_DIR_PIN, HIGH );
-        digitalWrite( RIGHT_MOTOR_DIR_PIN, LOW );
-        analogWrite( LEFT_MOTOR_PWM_PIN, 128 );
-        analogWrite( RIGHT_MOTOR_PWM_PIN, 128 );
+        digitalWrite( LEFT_MOTOR_DIR_PIN, LOW );
+        digitalWrite( RIGHT_MOTOR_DIR_PIN, HIGH );
+        leftMotor = LEFT_MOTOR_SPEED;
+        rightMotor = RIGHT_MOTOR_SPEED;
         break;
 
       case 'b':
         // Двигаемся назад
         digitalWrite( LEFT_MOTOR_DIR_PIN, LOW );
         digitalWrite( RIGHT_MOTOR_DIR_PIN, LOW );
-        analogWrite( LEFT_MOTOR_PWM_PIN, LEFT_MOTOR_SPEED );
-        analogWrite( RIGHT_MOTOR_PWM_PIN, RIGHT_MOTOR_SPEED );
+        leftMotor = LEFT_MOTOR_SPEED;
+        rightMotor = RIGHT_MOTOR_SPEED;
         break;
 
       case 'u':
         // Камера вверх
-        pos = cameraUpDown.read();
-        pos += CAMERA_STEP;
-        if(pos < CAMERA_EXTREAM_UP) {
-          cameraUpDown.write(pos);
+        if(cameraUpDownServo < CAMERA_EXTREAM_UP) {
+          cameraUpDownServo++;
         }
-        Serial.println(pos);
+        Serial.println(cameraUpDownServo);
         break;
         
       case 'd':
-        // Камера вверх
-        pos = cameraUpDown.read();
-        pos -= CAMERA_STEP;
-        if(pos > CAMERA_EXTREME_DOWN) {
-          cameraUpDown.write(pos);
+        // Камера вниз
+        if(cameraUpDownServo > CAMERA_EXTREME_DOWN) {
+          cameraUpDownServo--;
         }
-        Serial.println(pos);
+        Serial.println(cameraUpDownServo);
         break;
 
       case 'q':
         // Камера влево
-        pos = cameraLeftRight.read();
-        pos += CAMERA_STEP;
-        if(pos < CAMERA_EXTREME_LEFT) {
-          cameraLeftRight.write(pos);
+        if(cameraLeftRightServo < CAMERA_EXTREME_LEFT) {
+          cameraLeftRightServo++;
         }
-        Serial.println(pos);
+        Serial.println(cameraLeftRightServo);
         break;
         
       case 'e':
         // Камера вправо
-        pos = cameraLeftRight.read();
-        pos -= CAMERA_STEP;
-        if(pos > CAMERA_EXTREME_RIGHT) {
-          cameraLeftRight.write(pos);
+        if(cameraLeftRightServo > CAMERA_EXTREME_RIGHT) {
+          cameraLeftRightServo--;
         }
-        Serial.println(pos);
+        Serial.println(cameraLeftRightServo);
         break;
 
       case 'w':
         // Камера по центру.
-        cameraLeftRight.write(CAMERA_CENTER);
-        cameraUpDown.write(CAMERA_STRAIGHT_FORWARD);
+        cameraLeftRightServo = CAMERA_CENTER;
+        cameraUpDownServo = CAMERA_STRAIGHT_FORWARD;
         break;
 
       case 's':
         // Останавливаемся.
-        digitalWrite( LEFT_MOTOR_DIR_PIN, HIGH );
-        digitalWrite( RIGHT_MOTOR_DIR_PIN, HIGH);
-        analogWrite( LEFT_MOTOR_PWM_PIN, 0 );
-        analogWrite( RIGHT_MOTOR_PWM_PIN, 0 );
+        leftMotor = 0;
+        rightMotor = 0;
         break;
 
     }
@@ -147,8 +183,10 @@ void loop()
   
   } else {
     // Если никакая команда не пришла, то поспать немного, чтобы зря не расходовать батарею.
-    delay(10);
+        Serial.println(cameraLeftRightServo);
+    delay(1000);
   }
 
 }
+
 
