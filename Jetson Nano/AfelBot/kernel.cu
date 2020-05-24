a74b4f6cbc1bccc15254d48377757c39f0db79a9
Vitaliy Tsirkunov <vitaliy@tsirkunov.ru>
2020-05-24 13:02:27 +0300

    Переделал проект для Jetson Nano под CMake

--- Jetson Nano/AfelBot/kernel.cu
+++
@@ -1,9 +0,0 @@
-
-#include "cuda_runtime.h"
-#include "device_launch_parameters.h"
-
-#include <stdio.h>
-
-int main(void) {
-	printf("Hello World");
-}
---
+++ Jetson Nano/Jetson Nano.cpp
@@ -0,0 +1,12 @@
+﻿// Jetson Nano.cpp: определяет точку входа для приложения.
+//
+
+#include "Jetson Nano.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
